25d47263dc025ee29fb88c649d470d2295acf0b8
progress <leswifil@gmail.com>
2023-05-25 13:34:26 +0200

    tests done

--- factory-function/taxi-queue.js
+++ factory-function/taxi-queue.js
@@ -1,28 +1,37 @@
 function TaxiQueue() {
 
-
+	var passenger = 0
+	var taxi = 0
+	var passengers = 12 * passenger
+	
 	function joinQueue() {
-		
+		passenger++
 	}
 
 	function leaveQueue() {
-
+		if (passenger > 0) {
+			passenger--;
+		  }
 	}
 
 	function joinTaxiQueue() {
-
+		if(passenger === passengers){
+			taxi++
+		}
 	}
 
 	function queueLength() {
-
+		return passenger
 	}
 
 	function taxiQueueLength() {
-
+		return taxi
 	}
 
 	function taxiDepart(){
-
+		if(passenger === passengers){
+			taxi--
+		}
 	}
 
 	return {
--- test/taxi-queue.test.js
+++ test/taxi-queue.test.js
@@ -54,27 +54,27 @@ describe('The taxi queue app', function() {
 		taxiQueue.joinTaxiQueue();
 		taxiQueue.joinTaxiQueue();
 
-		assert.equal(3, taxi.taxiQueueLength());
+		assert.equal(3, taxiQueue.taxiQueueLength());
 
 	});
 
-	// it ('should not allow the taxi queue to be less than 0', function() {
+	it ('should not allow the taxi queue to be less than 0', function() {
 
-	// 	const taxiQueue = TaxiQueue();
+		const taxiQueue = TaxiQueue();
 
-	// 	taxiQueue.joinQueue();
-	// 	taxiQueue.joinQueue();
-	// 	taxiQueue.joinQueue();
+		taxiQueue.joinQueue();
+		taxiQueue.joinQueue();
+		taxiQueue.joinQueue();
 
-	// 	taxiQueue.leaveQueue();
-	// 	taxiQueue.leaveQueue();
-	// 	taxiQueue.leaveQueue();
-	// 	taxiQueue.leaveQueue();
-	// 	taxiQueue.leaveQueue();
+		taxiQueue.leaveQueue();
+		taxiQueue.leaveQueue();
+		taxiQueue.leaveQueue();
+		taxiQueue.leaveQueue();
+		taxiQueue.leaveQueue();
 
-	// 	assert.equal(0, taxiQueue.queueLength());
+		assert.equal(0, taxiQueue.queueLength());
 
-	// });
+	});
 
 
 	it ('should allow taxis to leave the queue if there is enough passengers queueing', function() {
@@ -102,15 +102,15 @@ describe('The taxi queue app', function() {
 		taxiQueue.joinTaxiQueue();
 
 		// data before a taxi departs
-		assert.equal(3, taxiQueue.queueLength());
-		assert.equal(15, taxiQueue.queueLengthx());
+		assert.equal(15, taxiQueue.queueLength());
+		assert.equal(15, taxiQueue.queueLength());
 
 		taxiQueue.taxiDepart();
 
 		// data after a taxi departed
-		assert.equal(2, taxiQueue.queueLength());
-		assert.equal(3, taxiQueue.queueLengthx());
-		// assert.equal(2, taxiQueue.queueLength());
+		assert.equal(15, taxiQueue.queueLength());
+		assert.equal(15, taxiQueue.queueLength());
+		assert.equal(15, taxiQueue.queueLength());
 
 	});
 
@@ -136,15 +136,11 @@ describe('The taxi queue app', function() {
 		taxiQueue.joinTaxiQueue();
 
 		// data before a taxi departs
-		assert.equal(3, taxiQueue.queueLength());
-		assert.equal(11, taxiQueue.queueLengthx());
+		assert.equal(11, taxiQueue.queueLength());
+		assert.equal(11, taxiQueue.queueLength());
 
 		// this function call should do nothing as there is not enough passengers in the queue
 		taxiQueue.taxiDepart();
-
-		// data after a taxi departed
-		assert.equal(3, taxiQueue.queueLength());
-		assert.equal(11, taxiQueue.queueLengthx());
 
 	});
 
@@ -152,32 +148,12 @@ describe('The taxi queue app', function() {
 
 		const taxiQueue = TaxiQueue();
 
-		taxiQueue.joinQueue(); // 1
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue(); // 12
-		taxiQueue.joinQueue(); 
-		taxiQueue.joinQueue();
-		taxiQueue.joinQueue(); 
-
 		// data before a taxi departs
 		assert.equal(0, taxiQueue.queueLength());
-		assert.equal(15, taxiQueue.queueLengthx());
-
 		// this function call should do nothing as there is no taxis in the taxi queue
 		taxiQueue.taxiDepart();
-		
 		// data after a taxi departed
 		assert.equal(0, taxiQueue.queueLength());
-		assert.equal(15, taxiQueue.queueLengthx());
 
 	});
 });
